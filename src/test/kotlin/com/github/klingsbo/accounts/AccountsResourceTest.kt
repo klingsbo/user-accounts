527c313cd8a1aa6f3e1c92a095711cfc17b523fe
D. P <dj0l33x@gmail.com>
2023-06-15 18:59:06 +0300

    reformatting

--- src/test/kotlin/com/github/klingsbo/accounts/AccountsResourceTest.kt
+++ src/test/kotlin/com/github/klingsbo/accounts/AccountsResourceTest.kt
@@ -2,8 +2,13 @@ package com.github.klingsbo.accounts
 
 import io.quarkus.test.common.http.TestHTTPEndpoint
 import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import io.restassured.http.Header
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.notNullValue
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.util.*
 
 @QuarkusTest
 @TestHTTPEndpoint(AccountsResource::class)
@@ -11,6 +16,32 @@ class AccountsResourceTest {
 
     @Test
     fun `create new account successfully`() {
+        val testEmail = "${UUID.randomUUID()}@mail.com"
+        given()
+            .header(Header("Content-Type", "application/json"))
+            .header(Header("Accept", "application/json"))
+            .body("{\"email\": \"$testEmail\"}")
+            .`when`().post()
+            .then()
+            .statusCode(201)
+            .body("id", notNullValue())
+            .body("createdAt", notNullValue())
+            .body("email", equalTo(testEmail))
 
+        Account.findByEmail(testEmail)!!
+    }
+
+    @Test
+    fun `create new account return conflicts`() {
+        val testEmail = "${UUID.randomUUID()}@mail.com"
+        Account().apply { email = testEmail }.persist()
+
+        given()
+            .header(Header("Content-Type", "application/json"))
+            .header(Header("Accept", "application/json"))
+            .body("{\"email\": \"$testEmail\"}")
+            .`when`().post()
+            .then()
+            .statusCode(409)
     }
 }
